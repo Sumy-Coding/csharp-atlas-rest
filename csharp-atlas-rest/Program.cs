068a4901d585918dfc924229ebf741aa05edbb4a
malandr <quadr988@gmail.com>
2022-08-07 21:55:45 +0300

    HTML

--- csharp-atlas-rest/Program.cs
+++ csharp-atlas-rest/Program.cs
@@ -1,9 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Net.Http.Headers;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using csharp_atlas_rest;
+using HtmlAgilityPack;
 
 namespace Atlas
 {
@@ -13,11 +15,27 @@ namespace Atlas
 
         static async Task Main(string[] args)
         {
-            client.DefaultRequestHeaders.Add("", "");
+            client.DefaultRequestHeaders.Add("country", "Ukraine");
             var resp = client.GetAsync("http://example.com");
-            var body = resp.Result.Content;
+            var body = resp.Result.Content.ReadAsStringAsync();
 
-            Console.WriteLine(resp);
+            // var resStream =resp.Result.Content.ReadAsStream();
+            // var readInt = resStream.ReadByte();
+
+            HtmlEncoder encoder = HtmlEncoder.Default;
+            TextWriter writer = new StringWriter();
+            encoder.Encode(writer, body.Result);
+
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(body.Result);
+
+            List<string> elems = new List<string>();
+            foreach (var elem in document.DocumentNode.SelectNodes("//div"))
+            {
+                elems.Add(elem.InnerText);
+                Console.WriteLine(elems[0]);    
+            }
+            
         }
         
         // async void getAsyncStreamss()
