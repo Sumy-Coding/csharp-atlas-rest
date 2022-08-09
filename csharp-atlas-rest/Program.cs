cdeeb63b276bdeed13124362de33e575558c298a
malandr <quadr988@gmail.com>
2022-08-09 20:02:40 +0300

    Page services

--- csharp-atlas-rest/Program.cs
+++ csharp-atlas-rest/Program.cs
@@ -12,6 +12,8 @@ namespace Atlas
 
         static async Task Main(string[] args)
         {
+            var start =  System.DateTime.Now;
+            
             string URL = "http://localhost:7190";
             string CONTENT_REST_URL = "http://localhost:7190/rest/api/content";
             string user = "admin";
@@ -25,13 +27,20 @@ namespace Atlas
 
             // ===== create page
             PageService pageService = new PageService();
-            var resp = pageService.CreatePage(url:CONTENT_REST_URL, token, "Page 2", "a sdas as s ad", "CSHR1", "1212418");
+            // var resp = pageService.CreatePage(url:CONTENT_REST_URL, token, "Page 2", "a sdas as s ad", "CSHR1", "1212418");
+            // ==== create several pages
+            for (int i = 1; i < 15; i++)
+            {
+                var resp = pageService.CreatePage(url:CONTENT_REST_URL, token, "Page csharp" + i, "CSharp TEST " + i, "CSHR1", "1212418");
+                Console.WriteLine(resp);
+            }
             
             // ===== create space
             // SpaceService spaceService = new SpaceService();
             // var resp = spaceService.CreateSpace(URL, token, "CSHR1", "CSHR1");
            
-            Console.WriteLine(resp);
+            // Console.WriteLine(resp);
+            Console.WriteLine("*** The action took " + DateTime.Now.Subtract(start).Milliseconds);
         }
         
         // async void getAsyncStreamss()
